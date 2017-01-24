c7a9e35bffbf41a2fb7cec21b37ed163eab1c8a9
Mark Palfreeman <markpalfreeman@gmail.com>
2017-01-24 13:20:21 -0800

    Convert to React app (via create-react-app)

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='logo'>
+        <h1 className='logo-title'>
+          <span>Trust</span>
+          <span>the</span>
+          <span>Rooster</span>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
