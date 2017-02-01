0adf32f78cdd060c41c47ddf0a6059233c5daef3
Mark Palfreeman <markpalfreeman@gmail.com>
2017-02-01 14:06:07 -0800

    Separate Header, style route links and hovers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Link, Match, Miss } from 'react-router'
+import { BrowserRouter, Match, Miss } from 'react-router'
 import Logo from './components/Logo/Logo'
+import Header from './components/Header/Header'
 import './App.css'
 
 const App = () => (
   <BrowserRouter>
     <div className='ttr'>
-      <header>
-        <nav>
-          <Link to='/about'>About</Link>
-          <Link to='/game'>Game</Link>
-        </nav>
-      </header>
-      <main>
+      <Header />
+      <main className='ttr-body'>
         <Match exactly pattern='/' component={Logo} />
         <Match exactly pattern='/about' component={() => <h1>ABOUT!!</h1>} />
         <Match exactly pattern='/game' component={() => <h1>LET'S PLAY A GAME!!</h1>} />
-        <Miss component={() => <h1>404!!</h1>} />
+        <Miss component={Logo} />
       </main>
     </div>
   </BrowserRouter>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router'
+import './Header.css'
+import rooster from '../../img/rooster.svg'
+
+const Header = () => {
+  return (
+    <header className='ttr-header'>
+      <nav className='ttr-header-nav'>
+        <Link to='/' className='ttr-header-link'>
+          <img className='ttr-header-logo' src={rooster} alt='rooster logo' />
+        </Link>
+        <Link to='/about' className='ttr-header-link'>About</Link>
+        <Link to='/game'className='ttr-header-link'>Game</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
