9ed5d11510c6f9ab9a479565c3b260778c8a4bc7
Mark Palfreeman <markpalfreeman@gmail.com>
2017-02-01 14:58:03 -0800

    Swap out Router for react-router beta

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import { BrowserRouter, Match, Miss } from 'react-router'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Logo from './components/Logo/Logo'
 import Header from './components/Header/Header'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
+  <Router>
     <div className='ttr'>
       <Header />
       <main className='ttr-body'>
-        <Match exactly pattern='/' component={Logo} />
-        <Match exactly pattern='/about' component={() => <h1>ABOUT!!</h1>} />
-        <Match exactly pattern='/game' component={() => <h1>LET'S PLAY A GAME!!</h1>} />
-        <Miss component={Logo} />
+        <Switch>
+          <Route exact path='/' component={Logo} />
+          <Route path='/about' component={() => <h1>ABOUT!!</h1>} />
+          <Route path='/game' component={() => <h1>LET'S PLAY A GAME!!</h1>} />
+          <Route component={Logo} />
+        </Switch>
       </main>
     </div>
-  </BrowserRouter>
+  </Router>
 )
 
 export default App
